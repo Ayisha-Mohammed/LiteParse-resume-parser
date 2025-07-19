2ce5c2ba34c945f3ffddb2c1dd6090e8cfa25dc2
Ayisha <ca20ayisharukshana.m@gmail.com>
2025-07-19 22:19:39 +0530

    updated

--- app/services/parser_servies.py
+++ app/services/parser_servies.py
@@ -18,12 +18,12 @@ def extracted_text_from_docx(resume_file):
 def parse_resume(resume_file:FileStorage):
     file_name= resume_file.filename.lower()
     if file_name.endswith('.pdf'):
-       extracted_pdftext = extracted_text_from_pdf(resume_file)
-    if file_name.endswith('.docx'):
-       extracted_doctext = extracted_text_from_docx(resume_file)
+       text = extracted_text_from_pdf(resume_file)
+    if file_name.endswith('.docx'):      
+       text = extracted_text_from_docx(resume_file)
     else:
       return{"error":"Unsupported file format"}
     
-    return {"raw text:",  extracted_pdftext}
+    return {"raw text:",text}
 
     

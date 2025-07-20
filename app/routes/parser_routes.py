5ebf70e8b137b9e4e03fafad7fe34bffb060b323
Ayisha <ca20ayisharukshana.m@gmail.com>
2025-07-20 20:40:20 +0530

    updated

--- app/routes/parser_routes.py
+++ app/routes/parser_routes.py
@@ -1,12 +1,12 @@
 from flask import Blueprint , request, jsonify
-from app.services.parser_servies import parse_resume
+from app.services.res_parser import parse_resume
 
 parser_bp = Blueprint('parser', __name__)
 
+
 @parser_bp.route('/parse',methods =['POST'])
 def parse():
-    resume_file=request.files.get('resume')
-
+    resume_file=request.files.get('file')
     if not resume_file:
         return jsonify({'error':'No resume uploaded'}),400
     else:
--- app/services/res_parser.py
+++ app/services/res_parser.py
@@ -1,15 +1,17 @@
 
 from werkzeug.datastructures import FileStorage
-from utils.ext_text import extracted_text_from_pdf,extracted_text_from_docx
-from utils.ext_data import extract_email,extract_phone,extract_name
-
+from app.utils.ext_text import extracted_text_from_pdf,extracted_text_from_docx
+from app.utils.ext_data import extract_email,extract_phone,extract_name
 
 
 def parse_resume(resume_file:FileStorage):
+    print("Filename:", resume_file.filename)
+    print("Content-Type:", resume_file.content_type)
+
     file_name= resume_file.filename.lower()
     if file_name.endswith('.pdf'):
        text = extracted_text_from_pdf(resume_file)
-    if file_name.endswith('.docx'):      
+    elif file_name.endswith('.docx'):      
        text = extracted_text_from_docx(resume_file)
     else:
       return{"error":"Unsupported file format"}

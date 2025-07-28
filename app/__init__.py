6bd9cb38fe42fe926950d6808d1b4cbf6514f600
Ayisha <ca20ayisharukshana.m@gmail.com>
2025-07-28 17:08:30 +0530

    dependency updated

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,13 @@
 from flask import Flask
 from app.routes.parser_routes import parser_bp
 from .limiter import limiter
+import logging
 
 
 def create_app():
     app= Flask(__name__)
+    logging.basicConfig(level=logging.INFO)
+    app.logger.setLevel(logging.INFO)
     limiter.init_app(app)
     app.register_blueprint(parser_bp)
     return app
--- app/routes/parser_routes.py
+++ app/routes/parser_routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint , request, jsonify
+from flask import Blueprint , request, jsonify , current_app
 from app.services.res_parser import parse_resume
 from app.limiter import limiter 
 
@@ -11,6 +11,7 @@ def parse():
         resume_file = request.files.get('file')
         if not resume_file:
             return jsonify({'success': False, 'error': 'No resume uploaded'}), 400
+        current_app.logger.info(f"Uploaded file: {resume_file.filename}")
         print("Uploaded Your file:", resume_file.filename)
         result_of_parsed_resume = parse_resume(resume_file)
 
@@ -23,3 +24,8 @@ def parse():
     except Exception as e:
         print("Unexpected error:", e)
         return jsonify({'success': False, 'error': 'Internal Server Error', 'details': str(e)}), 500
+
+
+@parser_bp.route('/ping', methods=['GET'])
+def ping():
+    return jsonify({"success": True, "message": "API is alive"}), 200

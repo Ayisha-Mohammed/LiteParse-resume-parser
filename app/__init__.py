c591469a3d8fb132263d39f3e8f90f77d501ea33
Ayisha <ca20ayisharukshana.m@gmail.com>
2025-07-29 08:59:42 +0530

    updated

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,7 @@ from flask_cors import CORS
 
 def create_app():
     app= Flask(__name__)
-    CORS(app)
+    CORS(app, resources={r"/*": {"origins": "*"}}, supports_credentials=True)
     logging.basicConfig(level=logging.INFO)
     app.logger.setLevel(logging.INFO)
     limiter.init_app(app)

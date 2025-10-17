93bcacd88b1d4ef57df515472f4566aae6b67ba3
Ayisha <ca20ayisharukshana.m@gmail.com>
2025-10-17 10:19:53 +0530

    home update

--- run.py
+++ run.py
@@ -1,7 +1,17 @@
+# from app import create_app
+
+# app = create_app()
+
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
+
+import os
 from app import create_app
 
 app = create_app()
 
-
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Render provides this
+    app.run(host="0.0.0.0", port=port)
+
